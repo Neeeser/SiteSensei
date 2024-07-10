7727227f1986b798645e29a8d259a5d6bd83ab2b
neeeser <andrew.neeser@me.com>
2024-07-10 12:50:31 -0400

    chore: Add meta tags and title to RootLayout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,17 @@
-// app/layout.jsx
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { ThemeProvider } from 'next-themes';
 import Navbar from '../components/Navbar';
-import './globals.css'
+import Head from 'next/head';
+import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Site Sensei</title>
+        <meta name="description" content="Generate interactive code for your website quickly" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <UserProvider>
         <ThemeProvider attribute="class">
           <body className="flex flex-col h-screen">
